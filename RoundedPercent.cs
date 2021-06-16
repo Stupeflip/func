79908510e1d59a767e7dd31c4fe1039d115ea890
Stupeflip <40721820+Stupeflip@users.noreply.github.com>
2021-06-16 16:42:45 +0200

    Update RoundedPercent.cs

--- RoundedPercent.cs
+++ RoundedPercent.cs
@@ -1,4 +1,4 @@
-static double RoundedPercent(double x, double y, int splitPos)
+static double RoundedPercent(double x, double y, int splitPos = 0)
 {
     if (x > 0 || y > 0)
         return (double)Math.Round(Math.Min(x, y) / Math.Max(x, y), splitPos, MidpointRounding.AwayFromZero);
